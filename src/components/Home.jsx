dcaf08fceb3c467475fd3253369321c862406845
hanyn1 <hanine.azizi@esprit.tn>
2024-01-02 09:28:20 +0100

    first step

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    //should contain header you will find an exemple in the assets folder
+  return (
+    <div>
+      
+    </div>
+  )
+}
